4457848142c6f55b3d1a5df7ec043a6f77fd6f7d
Daniil Vladimirov <porvipop@mail.ru>
2023-01-22 13:58:35 +0300

    Add CoroutineModule, provide IO dispatcher

--- app/src/main/java/com/example/binchecker/data/repository/Repository.kt
+++ app/src/main/java/com/example/binchecker/data/repository/Repository.kt
@@ -1,9 +1,10 @@
 package com.example.binchecker.data.repository
 
+import com.example.binchecker.di.ApplicationScope
 import com.example.binchecker.domain.model.Bank
 import com.example.binchecker.domain.repository.LocalDataSource
 import com.example.binchecker.domain.repository.RemoteDataSource
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -11,23 +12,24 @@ import javax.inject.Inject
 class Repository @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource,
-) {
+    @ApplicationScope private val iODispatcher: CoroutineDispatcher
+    ) {
 
 
     fun getAllBanks(): Flow<List<Bank>> {
         return localDataSource.getAllBanks()
     }
 
-     suspend fun getApiResponse(bin: String) = withContext(Dispatchers.IO) {
+     suspend fun getApiResponse(bin: String) = withContext(iODispatcher) {
          return@withContext remoteDataSource.getApiResponse(bin = bin)
     }
 
-     suspend fun deleteAllBanks() = withContext(Dispatchers.IO) {
+     suspend fun deleteAllBanks() = withContext(iODispatcher) {
         localDataSource.deleteAllBanks()
     }
 
 
-    suspend fun deleteBank(bank: Bank) = withContext(Dispatchers.IO) {
+    suspend fun deleteBank(bank: Bank) = withContext(iODispatcher) {
         localDataSource.deleteBank(bank = bank)
     }
 
